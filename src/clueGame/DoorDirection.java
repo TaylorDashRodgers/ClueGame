9ba826a1b895fbaf97005e88581de5756149cdba
Andre Reynolds <amreynolds@mines.edu>
2022-03-14 12:57:08 -0600

    got rid of uneccesary methods in the DoorDirection enum type

--- src/clueGame/DoorDirection.java
+++ src/clueGame/DoorDirection.java
@@ -1,16 +1,7 @@
 package clueGame;
 
 public enum DoorDirection {
-	UP("^"), LEFT("<"), RIGHT(">"), DOWN("v");
-	private String value;
-	
-	DoorDirection (String aValue){
-		value = aValue;
-	}
-	
-	public String toString() {
-		return value;
-	}
+	UP, LEFT, RIGHT, DOWN;
 	
 }
 
