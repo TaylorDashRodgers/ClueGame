e12539441cb56dd773ff46273e7d98c0febf2378
Andre Reynolds <amreynolds@mines.edu>
2022-04-09 17:58:01 -0600

    refactored

--- src/clueGame/GameCards.java
+++ src/clueGame/GameCards.java
@@ -11,14 +11,15 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class GameCards extends JPanel{
+    // Variable declaration
 	public JPanel People, Rooms, Weapons, peopleCardsHand, roomCardsHand, weaponCardsHand, peopleCardsSeen, roomCardsSeen, weaponCardsSeen;
 	private JLabel inHandPeople, inHandRooms, inHandWeapons, seenPeople, seenRooms, seenWeapons;
-    //private JTextField card;
 	
 	private static Board board;
 	
 
     public GameCards() {
+    	setBorder(new TitledBorder(new EtchedBorder(), "Known Cards:"));
         setLayout(new GridLayout(3,0));
         People = new JPanel();
         People.setLayout(new GridLayout(4,0));
@@ -64,6 +65,7 @@ public class GameCards extends JPanel{
         add(Weapons);
     }
     
+    // Creates the add to the hand methods.
     public void addPersonHand(Card card) {
     	peopleCardsHand.add(new JTextField(card.getCardName()));
     	peopleCardsHand.revalidate();
@@ -78,6 +80,7 @@ public class GameCards extends JPanel{
     	weaponCardsHand.revalidate();
     }
 
+    // Creates the add to the seen methods.
     public void addPersonSeen(Card card) {
     	peopleCardsSeen.add(new JTextField(card.getCardName()));
     	peopleCardsSeen.revalidate();
@@ -95,15 +98,12 @@ public class GameCards extends JPanel{
 		GameCards panel = new GameCards();  // create the panel
 		JFrame frame = new JFrame();  // create the frame 
 		frame.setContentPane(panel); // put the panel in the frame
-		frame.setSize(180, 750);  // size the frame
+		frame.setSize(160, 750);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 
-		// Board is singleton, get the only instance
 		board = Board.getInstance();
-		// set the file names to use my config files
 		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
-		// Initialize will load BOTH config files
 		board.initialize();
 		
 		Player test = board.getPlayers().get(0);
@@ -121,6 +121,7 @@ public class GameCards extends JPanel{
 			}
 		}
 		
+		// Grabs cards from two other players to test the seen funcitonality
 		for (Card temp : test2.getHand() ) {
 			test.getSeen().add(temp);
 		}
